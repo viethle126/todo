4f5d7fc0f8aa616cfc612d49b60b87e0f886dbf1
Viet Le <viethle126@gmail.com>
2016-04-20 01:27:40 -0700

    Completed Exercise 130: Front End Testing

---
+++ public/app.spec.js
@@ -0,0 +1,64 @@
+var page = require('webpage').create();
+var url = 'http://localhost:1337';
+
+page.viewportSize = { width: 1920, height: 1080 };
+
+page.onConsoleMessage = function(message) {
+  console.log(message);
+}
+
+page.open(url, function start(status) {
+  page.includeJs(
+    'http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js',
+    function() {
+      page.evaluate(function() {
+        setTimeout(function() {
+          $('a')[0].click();
+        }, 2000);
+        setTimeout(function() {
+          $('a')[1].click();
+        }, 4500);
+        setTimeout(function() {
+          angular.element(document.body).scope().$apply(
+            function() {
+              vm.task = 'PhantomJS Task';
+              vm.date = vm.today;
+            }
+          );
+          $("button:contains('Add')").click()
+        }, 6500);
+        setTimeout(function() {
+          $("button:contains('PhantomJS Task')").click();
+        }, 8000);
+        setTimeout(function() {
+          $("button:contains('Clear Completed')").click()
+        }, 9500);
+      })
+    }
+  )
+})
+
+setTimeout(function() {
+  page.render('phantom/01.landing.jpg', { format: 'jpg', quality: '100' });
+}, 1500);
+
+setTimeout(function() {
+  page.render('phantom/02.profile.jpg', { format: 'jpg', quality: '100' });
+}, 4000);
+
+setTimeout(function() {
+  page.render('phantom/03.todo.jpg', { format: 'jpg', quality: '100' });
+}, 6000);
+
+setTimeout(function() {
+  page.render('phantom/04.added.jpg', { format: 'jpg', quality: '100' });
+}, 7500);
+
+setTimeout(function() {
+  page.render('phantom/05.completed.jpg', { format: 'jpg', quality: '100' });
+}, 9000);
+
+setTimeout(function() {
+  page.render('phantom/06.cleared.jpg', { format: 'jpg', quality: '100' });
+  phantom.exit();
+}, 10500);
